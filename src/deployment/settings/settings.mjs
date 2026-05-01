670eb48bd37f198dbf53e4fb9cca0fbe3efbc3fa
SirFizX <eeisaman@auburnschl.edu>
2026-05-01 10:45:42 -0400

    netlify

--- src/deployment/settings/settings.mjs
+++ src/deployment/settings/settings.mjs
@@ -1,21 +1,8 @@
 /** @type {import('../types/settings').DeploymentSettings<'render.com'>} */
 const deploymentSettings = {
-  host: 'render.com',
-  type: 'web-service',
-  services: [
-    {
-      name: 'api',
-      type: 'node',
-      routePrefix: '/api',
-      localPort: 8787
-    },
-    {
-      name: 'multiplayer',
-      type: 'go',
-      routePrefix: '/api/multiplayer',
-      localPort: 5000
-    }
-  ],
+  host: 'netlify',
+  type: 'static',
+  services: [],
   static: {
     basePath: '/'
   }
